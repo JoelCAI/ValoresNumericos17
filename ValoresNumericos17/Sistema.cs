0c25f7564845c66390609e7edf34070d30d5ce76
JoelCAI <joelargentina12345@gmail.com>
2022-04-08 14:19:48 -0300

    CAMBIO4

--- ValoresNumericos17/Sistema.cs
+++ ValoresNumericos17/Sistema.cs
@@ -23,16 +23,20 @@ namespace ValoresNumericos17
             numeroDos = Validador.PedirNumeroDecimal("\n Ingrese el segundo valor numérico.");
 
   
-            decimal suma = numeroUno + numeroDos;
-            decimal resta = numeroUno - numeroDos;
-            decimal multiplicacion = numeroUno * numeroDos;
-            decimal division = numeroUno / numeroDos;
-            decimal resto = numeroUno % numeroDos;
+            decimal suma = decimal.Round((numeroUno + numeroDos),4);
+            decimal resta = decimal.Round((numeroUno - numeroDos), 4);
+            decimal multiplicacion = decimal.Round((numeroUno * numeroDos), 4);
+            decimal division = decimal.Round((numeroUno / numeroDos), 4);
+            decimal resto = decimal.Round((numeroUno % numeroDos), 4);
 
             double numeroUnoPotencia = Convert.ToDouble(numeroUno);
             double numeroDosPotencia = Convert.ToDouble(numeroDos);
 
             double potencia = Math.Pow(numeroUnoPotencia, numeroDosPotencia);
+
+            decimal numeroPotencia = Convert.ToDecimal(potencia);
+            
+            decimal numeroFinalPotencia = decimal.Round(numeroPotencia, 4);
 
             Console.Clear();
             Console.WriteLine(" \n La suma de *" + numeroUno + "* más" + " *" + numeroDos + "* es: *" + suma + "*");
@@ -40,7 +44,7 @@ namespace ValoresNumericos17
             Console.WriteLine(" \n La Multiplicación de *" + numeroUno + "* por" + " *" + numeroDos + "* es: *" + multiplicacion + "*");
             Console.WriteLine(" \n La División de *" + numeroUno + "* entre" + " *" + numeroDos + "* es: *" + division + "*");
             Console.WriteLine(" \n El Resto de *" + numeroUno + "* entre" + " *" + numeroDos + "* es: *" + resto + "*");
-            Console.WriteLine(" \n La Potencia de *" + numeroUno + "* elevado a la" + " *" + numeroDos + "* es: *" + potencia + "*");
+            Console.WriteLine(" \n La Potencia de *" + numeroUno + "* elevado a la" + " *" + numeroDos + "* es: *" + numeroFinalPotencia + "*");
 
             Console.WriteLine(" \n Si el resultado no fue el esperado es porque ignoró la recomendación e ingreso" +
                               " un punto en vez de una coma");
